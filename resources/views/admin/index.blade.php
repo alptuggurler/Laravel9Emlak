230e357b62b5a4987bac144275570b59bdd0120b
alptuggurler <alptug.gurler@gmail.com>
2022-05-18 15:08:02 +0300

    commit bozuldu

--- resources/views/admin/index.blade.php
+++ resources/views/admin/index.blade.php
@@ -9,7 +9,16 @@
     <div class="main-panel">
 
         <div class="content-wrapper">
-            <h1>test2222</h1>
+
+            <div class="page-header">
+                <h3 class="page-title"> Blank Page </h3>
+                <nav aria-label="breadcrumb">
+                    <ol class="breadcrumb">
+                        <li class="breadcrumb-item"><a href="/">Home</a></li>
+                        <li class="breadcrumb-item active" aria-current="page">blank</li>
+                    </ol>
+                </nav>
+            </div>
 
 
 
