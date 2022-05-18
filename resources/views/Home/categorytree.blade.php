b7ded3afd3d271a7add70398ef6e73e237951cf0
alptuggurler <alptug.gurler@gmail.com>
2022-05-18 15:06:31 +0300

    commit bozuldu

--- resources/views/Home/categorytree.blade.php
+++ resources/views/Home/categorytree.blade.php
@@ -1,8 +1,18 @@
 @foreach($children as $subcategory)
     <div class="dropdown">
-        @if(count($subcategory->$children))
-            <a class="dropdown-item " href="property-single.html">{{$subcategory->title}}</a>
-        @endif
 
+
+        @if(count($subcategory->children))
+            <p1>test2</p1>
+            <a class="dropdown-item " href="property-single.html">{{$subcategory->title}}</a>
+            <ul>
+                @include('home.categorytree',['children'=> $subcategory->$children])
+            </ul>
+        <hr>
+        @else
+            <a class="dropdown-item " href="{{route('categoryhouse',['id'=>$subcategory->id, 'slug'=>$subcategory->title])}}">{{$subcategory->title}}</a>
+        @endif
     </div>
+
+
 @endforeach
